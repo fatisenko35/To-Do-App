2e28222455e2e6754afaae06aaafc688bf7df0e3
fatisenko <fatihsenko@gmail.com>
2022-02-16 14:03:22 +0300

    Html Css JS Structure Done

---
+++ App.js
@@ -0,0 +1,22 @@
+let inputAdd = document.getElementById("add-input");
+let buttonAdd = document.querySelector("#add-button");
+let containerAdd = document.querySelector(".contaniner");
+
+
+buttonAdd.addEventListener("click", ()=>{
+    const todoAdd = document.createElement("button");
+    todoAdd.innerHTML = inputAdd.value;
+    document.querySelector(".container").appendChild(todoAdd);
+
+    todoAdd.addEventListener("click",()=>{
+        todoAdd.remove();
+        const doneList = document.createElement("button");
+        doneList.innerHTML = todoAdd.innerHTML;
+        document.querySelector(".done").appendChild(doneList);
+
+        doneList.addEventListener("click", () => {
+            doneList.remove();
+        })
+    });
+});
+
